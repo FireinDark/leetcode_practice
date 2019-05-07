234bb57299e5e2855e51eea6ed5f72c75a9bfc0a
fireindark <993378951@qq.com>
2019-05-07 11:59:11 +0800

    hanmming distance

--- 23_merge_k_sorted_lists.py
+++ 23_merge_k_sorted_lists.py
@@ -99,38 +99,37 @@ class ListNode:
 
 
 class Solution:
-    # def mergeKLists(self, lists: List[ListNode]) -> ListNode:
-    #     if not lists: return
-    #     n = len(lists)
-    #     return self.merge(lists, 0, n - 1)
+    def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        if not lists: return
+        n = len(lists)
+        return self.merge(lists, 0, n - 1)
 
     def merge(self, lists, left, right):
-        print("传入参数", lists, left, right)
+        # print("传入参数", lists, left, right)
         if left == right:
             return lists[left]
         mid = left + (right - left) // 2
         l1 = self.merge(lists, left, mid)
-        print("L1", l1)
+        # print("L1", l1)
         l2 = self.merge(lists, mid + 1, right)
-        print("l2", l2)
+        # print("l2", l2)
 
         return self.mergeTwoLists(l1, l2)
 
     def mergeTwoLists(self, l1, l2):
         if not l1: return l2
         if not l2: return l1
-        return l1 + l2
-        # if l1.val < l2.val:
-        #     l1.next = self.mergeTwoLists(l1.next, l2)
-        #     return l1
-        # else:
-        #     l2.next = self.mergeTwoLists(l1, l2.next)
-        #     return l2
+        if l1.val < l2.val:
+            l1.next = self.mergeTwoLists(l1.next, l2)
+            return l1
+        else:
+            l2.next = self.mergeTwoLists(l1, l2.next)
+            return l2
 
 
 if __name__ == '__main__':
     a = [[1], [2], [3], [4], [5]]
-    print("最终结果",Solution().merge(a, 0, 4))
+    print("最终结果", Solution().merge(a, 0, 4))
 
 
 
